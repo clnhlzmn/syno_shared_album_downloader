5d3099b90621cde0c616e9b8a8668e08def47660
Colin <colin.holzman@protonmail.com>
2023-12-10 15:35:45 -0500

    check args

--- syno_shared_album_downloader/__main__.py
+++ syno_shared_album_downloader/__main__.py
@@ -3,6 +3,9 @@ from sys import argv, exit
 from .download import download
 
 if __name__ == "__main__":
+    if len(argv) != 3:
+        print("Usage python -m syno_shared_album_downloader <sharing url> <output path>")
+        exit(-1)
     try:
         download(argv[1], argv[2])
         exit(0)
